0bfdc32bec5d6c8e2377422978f905a472a96f33
Tafsir <ht0nsc@splm.siemens.com>
2020-11-15 16:52:13 -0600

    basic post and get setup

--- Bot.py
+++ Bot.py
@@ -1,11 +1,11 @@
-from flask import Flask
-import os
+from flask import Flask ,request,redirect,url_for
+
 
 app = Flask(__name__)
 # Port number is required to fetch from env variable
 # http://docs.cloudfoundry.org/devguide/deploy-apps/environment-variable.html#PORT
 
-
+Json = "NULL"
 #cf_port = os.getenv("PORT")
 
 # Only get method by default
@@ -13,6 +13,22 @@ app = Flask(__name__)
 def hello():
     return 'Hello World'
 
+@app.route("/Modify", methods = ["POST", "GET"])
+def Modify():
+
+    if request.method == "POST":
+        JSON = "It Worked"
+        return redirect(url_for("Modify_Return", Json = JSON))
+    else:
+        if Json == "NULL":
+            return redirect(url_for("Modify_Return", Json = "Json is null"))
+        else:
+            return redirect(url_for("Modify_Return", Json = JSON))
+
+@app.route("/<Json>")
+def Modify_Return(Json):
+    return Json
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8080)
-    app.run(debug=True)
+     app.run(host='0.0.0.0',port=8080,debug=True)
+# app.run(debug=True)
