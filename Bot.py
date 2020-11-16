a3ea7f059c8d5035b427a283ba4ca4251b45efd5
Tafsir <ht0nsc@splm.siemens.com>
2020-11-15 19:23:36 -0600

    adding capability to get the database login credential from a text file

--- Bot.py
+++ Bot.py
@@ -1,5 +1,5 @@
-from flask import Flask ,request,redirect,url_for
-
+from flask import Flask ,request,redirect,url_for, jsonify
+import json
 
 app = Flask(__name__)
 # Port number is required to fetch from env variable
@@ -7,6 +7,20 @@ app = Flask(__name__)
 
 Json = "NULL"
 #cf_port = os.getenv("PORT")
+
+#pull credentials for database externally
+def read_file():
+    #This is the file location for my database information. Replace with your database information.
+    #File location '../database_folder/database.txt'
+    with open('../database_folder/database.txt', 'r') as f:
+        f_content = f.read()
+        f_content = f_content.split()
+
+        username = f_content[0]
+        password = f_content[1]
+
+        return username, password
+
 
 # Only get method by default
 @app.route('/')
@@ -17,8 +31,36 @@ def hello():
 def Modify():
 
     if request.method == "POST":
-        JSON = "It Worked"
-        return redirect(url_for("Modify_Return", Json = JSON))
+
+        value = request.json
+        #Brackets t specify an object in an object example: request.json['Object']
+
+        # Converts to Json
+        json_str = json.dumps(value)
+
+        #converts to a dictionary
+        value = json.loads(json_str)
+        JSON = value
+
+        #Test file system
+        username, password = read_file()
+
+        #Test output for further modification
+        output = {
+            "input" : json.dumps(value),
+            "postrgres_login" : {
+                "Username": username,
+                "Password": password}
+
+         }
+
+        output = json.dumps(output)
+
+        return output         #value['Stores']['Target']
+
+        #value['Stores']['Target'] ----> cheks the object stores for value Target
+
+        #return redirect(url_for("Modify_Return", Json = JSON))
     else:
         if Json == "NULL":
             return redirect(url_for("Modify_Return", Json = "Json is null"))
